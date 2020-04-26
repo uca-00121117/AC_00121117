f02cc2c82c3cf8689949588290d7b64af2cc828b
DARUBI <dilverpro@gmail.com>
2020-04-25 19:37:20 -0600

    Terminado :comments extra

--- tarea_2_00121117.asm
+++ tarea_2_00121117.asm
@@ -37,22 +37,26 @@ section .data
   
 ;Ejercicio 2
 ;El presidente Bayib Nukele, de un país ficticio, necesita presentar datos de una proyección de casos de la pandemia COVID-19, para lo cual su equipo de expertos ha encontrado la maravillosa y precisa formula precisa: “comenzando con dos casos el día uno, cada tres días se duplica el número”. Usted es el encargado de las gráficas, tiene que dejar una buena impresión. De manera dinámica, coloque 11 estimaciones partiendo del día 3, de tres en tres es decir primera celda día 3, segunda día 6, y así; una estimación por cada una o dos celdas de memoria, desde la celda 210h llenando toda la fila, cuando pase de 255 necesitará usar dos celdas de memoria para guardar el número.
-        mov     ax, 0000h
-        mov     al, 2d
-        mov     bx, 210h
-        mov     cx, 2d
-est:    mul     cx
-        mov     [bx], ax
-        cmp     ah, 00h
-        ja      two
-        je      one
-two:    add     bx, 2h
-        jmp     sig
-one:    add     bx, 1h
-sig:    cmp     bx, 21Fh
-        jb      est
+        mov     ax, 0d   ;comenzando
+        mov     al, 2d      ;dando valor de 2 a al 
+        mov     bx, 210h    ;dando el inicio del bucle 2
+        mov     cx, 2d      ;dando a cx el valos de 2
+est:    mul     cx          ;multiplicando a ax cx (2*2)
+        mov     [bx], ax    ;dando a la posicion 210h el resultado de ax*cx
+        cmp     ah, 00h     ;se le resta a ah
+        ja      dos         ;salta a dos
+        je      uno         ;salta a uno
+        ;Condicion 2 
+dos:    add     bx, 2h      ;suma a bx 2(posicion) 
+        jmp     sig         ;salta a siguiente
+        ;Condicion 1
+uno:    add     bx, 1h      ;sumando 1 en la posicion bx
+        ;salto hacia este paso
+sig:    cmp     bx, 21Fh    ;restando bx y 21Fh(posicion final)
+        jb      est         ;salto
 
  ;Ejercicio 3
+ ;Escriba de manera dinámica los primeros 16 números de la sucesión de Fibonacci, cada uno en una o dos celda de memoria, desde la celda 220h llenando toda la fila, cuando pase de 255 necesitará usar dos celdas de memoria para guardar el número. La sucesión obedece la siguiente formula:
         mov     cl, 0d          ;inicializando ax
         mov     [220h], cl      ;metiendo 0 en la primer casilla
         mov     cl, 1d          ;cambiando ax por 1
