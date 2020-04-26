2d63b0388aacde440b2cabd14c714e8b777f457d
DARUBI <dilverpro@gmail.com>
2020-04-25 19:08:30 -0600

    Terminado

--- tarea_2_00121117.asm
+++ tarea_2_00121117.asm
@@ -4,7 +4,7 @@ org 100h
 ; Tomando como base los últimos 5 dígitos de su carnet, cada digito representa una de las notas de las evaluaciones que serán la nota del primer parcial de una materia ficticia, por medio de un promedio, calcule la nota de su primer parcial ficticio y escriba en la fila de celdas de memoria de 200h a 20Fh el comentario correspondiente:
 section .text
 ;Iniciando en 0
-        mov ax, 0000h
+    mov ax, 0000h
 ;Sumando numeros    
 
     add ax, 2d
@@ -14,23 +14,22 @@ section .text
     add ax, 7d
     ; dando valor al total de numeros
     mov cl, 5d
-    ;dividiendo ax/cl (12/5)
+    ; dividiendo ax/cl (12/5)
     div cl
-    ;resultado es: Cociente 2 Residuo 2
-    ;Necesitamos cociente
-    ;A u n   s e   p a s a
-    ;1 2 3 4 5 6 7 8 9 A B 
+    ; resultado es: Cociente 2 Residuo 2
+    ; Necesitamos cociente
+    ; A u n   s e   p a s a
+    ; 1 2 3 4 5 6 7 8 9 A B 
 
     mov di ,0d
     mov cx,000h
-    mov cx ,[len]
+    mov cx ,[len];     me da bucle
+    mov cx , 11         ;longitud quemada 
     lupi:  
         mov bl,[comnt+di]
         mov [di+200h],  bl
         inc di
-        sub cx,1
     loop lupi
-    int 20h
 ;comprobamos que el cociente sea 2
 section .data
     comnt db "Aun se pasa"
@@ -53,9 +52,29 @@ one:    add     bx, 1h
 sig:    cmp     bx, 21Fh
         jb      est
 
+ ;Ejercicio 3
+        mov     cl, 0d          ;inicializando ax
+        mov     [220h], cl      ;metiendo 0 en la primer casilla
+        mov     cl, 1d          ;cambiando ax por 1
+        mov     [221h], cl      ;poniendo 1 en casilla 2
+        mov     bx, 222h        ;posicion de iniico ya que se "quemaron" los 2 primeros
+fibonacci: mov     ax, 0h       ;comenzando proceso
+        mov     [bx], ax        ;dando a bx lo que tiene  ax //primera iteracion ->222h = 0
+        ;regresando a f-2
+        sub     bx, 2h          ;restando 2 ala posicion // primera interaccion ->222h->220h
+        mov     al, [bx]        ;dando a al [bx] -> a al le da en la primer interacion 0 (222h-2h)
+        ;regresando a f-1
+        add     bx, 1h          ;sumando 1 ala posicion bx//primera -> 220h->221h
+        add     ax, [bx]        ;sumando a ax [bx]//Primera iteracion ax=0
+
+        add     bx, 1h          ;sumando 1  a la posicion de  bx para regresar al actual 221h->222h
+        mov     [bx], ax        ;dando a [bx]= ax -> 1 (posicion 221h)
+        add     bx, 1h          ;sumando 1 a la posicion de  bx para regresar al actual 222h->223h
+        cmp     bx, 22fh        ;comprobando que no sea la ultima casilla
+        jb      fibonacci            ;saltando si es menor
 
 
-
+int 20h
 
 
 
